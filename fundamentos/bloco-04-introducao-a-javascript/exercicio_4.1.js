c84c93f1872c103c6a754559721e1146b45b58e8
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-27 12:31:26 -0300

    Fazendo o primeiro exercicio de js!

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio_4.1.js
@@ -0,0 +1,10 @@
+const myName = "anna";
+const birthCity = "niteroi";
+let birthYear = 1995;
+console.log (myName);
+console.log (birthCity);
+birthYear = 2030;
+console.log (birthYear);
+birthCity = "rio";
+console.log (birthCity);
+// Não podemos aterar o valor de constantes!!//
