6e30792ae41477afaf093a4a24b8b3df35b2fe19
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-27 14:49:05 -0300

    Adicionando o terciro exercicio de js!

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio_4.3.js
@@ -0,0 +1,10 @@
+let notaDoCandidato = 55;
+if (notaDoCandidato >= 80) {
+    console.log ("Parabéns, você foi aprovado!");
+}
+else if (notaDoCandidato >= 60 && notaDoCandidato<=80){
+    console.log ("Você está na nossa lista de espera");
+}
+else {
+    console.log ("Você foi reprovada(o)");
+}
