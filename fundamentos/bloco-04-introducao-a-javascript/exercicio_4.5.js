62ac7ef87d0fb5b14dfee937d04a9b0b8d3c2f32
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-27 15:12:48 -0300

    Adicionando o quinto exercicio de js!

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio_4.5.js
@@ -0,0 +1,10 @@
+let weekDay = "domingo"
+let mensage;
+if (weekDay === "sabado" || weekDay === "domingo") {
+    mensage = "FINALMENTE, descanso merecido UwU";
+    console.log (mensage);
+}
+else {
+    mensage = "Oba, mais um dia de aprendizado na Trybe >:D";
+    console.log (mensage);
+}
