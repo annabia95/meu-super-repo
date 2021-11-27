ec034646aeeb470ced7e9f50e42968e90c8fb56b
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-27 15:40:16 -0300

    Adicionando o sexto exercicio

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio_4.6.js
@@ -0,0 +1,17 @@
+let estado = "reprovada";
+switch (estado){
+    case "aprovada":
+        console.log ("Parabéns, você foi aprovado!");
+        break;
+ 
+    case "lista":
+        console.log ("Você está na nossa lista de espera");
+        break;
+
+    case "reprovada":
+        console.log ("Você foi reprovada(o)");
+        break;
+
+default:
+        console.log ("não se aplica");
+}
