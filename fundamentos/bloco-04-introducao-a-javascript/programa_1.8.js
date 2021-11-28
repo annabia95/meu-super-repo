ce7a7ece15fb483d67a7ff3ab10a13d330340e47
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-28 20:44:02 -0300

    Adicionando o oitavo programa em js!

---
+++ fundamentos/bloco-04-introducao-a-javascript/programa_1.8.js
@@ -0,0 +1,5 @@
+let custoProduto = 20;
+let valorVenda = 30;
+let custoTotal = custoProduto + 0.2*custoProduto;
+let lucro = valorVenda - custoTotal;
+console.log ("O lucro deste produto foi de: " + lucro);
