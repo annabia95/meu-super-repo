3342d90b96b98dc6cf3a1ebea8346a8a12038148
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-28 09:09:40 -0300

    Adicionando o terceiro programa em js!

---
+++ fundamentos/bloco-04-introducao-a-javascript/programa_1.3.js
@@ -0,0 +1,12 @@
+const a = 50;
+const b = 30;
+const c = 20;
+if (a>b && a>c) {
+    console.log ("A constante a é maior do que b e do que c!")
+}
+else if (b>a && b>c){
+    console.log ("A constante b é maior do que a e do que c!")
+}
+else {
+    console.log ("A constante c é maior do que a e do que b!")
+}
