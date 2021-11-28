d1908461ba8aeddcd86e0fd1de3d7c50c0cd9636
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-28 09:02:49 -0300

    Adicionando o primeiro programa em js!

---
+++ fundamentos/bloco-04-introducao-a-javascript/programa_1.1.js
@@ -0,0 +1,8 @@
+let a = 4;
+let b = 2;
+console.log ("Resultado da adição = " + (a + b));
+console.log ("Resultado da subtração = " + (a - b));
+console.log ("Resultado da multiplicação = " + (a * b));
+console.log ("Resultado da divisão = " + (a / b));
+console.log ("Resultado do módulo = " + (a % b));
+
