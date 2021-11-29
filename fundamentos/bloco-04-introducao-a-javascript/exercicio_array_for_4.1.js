17b506ade6efc08c1009e2f1e70300eaaf8c1636
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-11-29 20:58:17 -0300

    Adicionando exercicio de array e for!

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio_array_for_4.1.js
@@ -0,0 +1,57 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// Exercicio 1
+//for (let index=0; index < numbers.length; index +=1) {
+//   console.log (numbers[index]);
+//}
+
+// Exercicio 2
+//let soma = 0;
+//for (let index=0; index < numbers.length; index +=1) {
+//   soma += numbers[index]
+//}
+//console.log (soma);
+
+// Exercicio 3
+//let soma = 0;
+//let media = 0;
+//for (let index=0; index < numbers.length; index +=1) {
+//    soma += numbers[index];
+//}
+//media = soma / numbers.length;
+//console.log (media);
+
+// Exercicio 4
+//let soma = 0;
+//let media = 0;
+//for (let index=0; index < numbers.length; index +=1) {
+//   soma += numbers[index];
+//}
+//media = soma / numbers.length;
+//if (media > 20) {
+//    console.log ("valor maior que 20");
+//}
+//else {
+//    console.log ("valor menor ou igual a 20");
+//}
+
+// Exercicio 5
+//let maior = 0;
+//for (let index=0; index < numbers.length; index +=1) {
+//        if (numbers[index] > maior) {
+//            maior = numbers[index];
+//        }
+//}
+//console.log (maior);
+
+// Exercicio 6
+let impar = 0;
+for (let index=0; index < numbers.length; index +=1) {
+       if (numbers[index]%2 != 0) {
+           impar +=1;
+      }
+}
+if (impar === 0) {
+    console.log ("nenhum valor impar encontrado");
+} else {
+    console.log (impar);
+}
