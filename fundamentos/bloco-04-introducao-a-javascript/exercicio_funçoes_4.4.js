4beec8850105e2e86a1ec5ba55df0de265d214a6
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-12-27 20:12:51 -0300

    Adicionando nova pasta

--- fundamentos/bloco-04-introducao-a-javascript/exercicio_funçoes_4.4.js
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio_funçoes_4.4.js
@@ -2,17 +2,43 @@
 
 //2 e 3 - Crie uma função que receba um array de inteiros e retorne o índice do maior e do menor valor.
 
-let inteiros = [2,4,8,1,3,10];
-let numeroMaior = 0;
-let numeroMenor = 0;
-function retornaMaiorMenor (inteiros){
-    for (let index = 0; index < inteiros.length; index +=1) {
-        if (inteiros[0] < inteiros[index]){
-            numeroMaior = inteiros[index]
-        } else {
-            numeroMenor = inteiros[index]
-        }
+let inteiros = [2,4,8,20,3,10];
+let indiceMaior = 0;
+function indiceDoMaior(numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMaior] < numeros[indice]) {
+        indiceMaior = indice;
+      }
     }
-return "O maior número é: " + numeroMaior + ". O menor número é: " + numeroMenor;
-}
-console.log (retornaMaiorMenor(inteiros));
+    return indiceMaior;
+  }
+console.log (indiceDoMaior(inteiros));
+
+//4- Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+//let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+//let nomeMaior = "a";
+//function retornaMaiorMenorNome (nomes){
+//    for (let index = 0; index < nomes.length; index +=1) {
+//          if (nomeMaior.length < nomes[index].length) {
+//            nomeMaior = nomes[index];
+//        }
+//    }
+//return "O maior nome é: " + nomeMaior;
+//}
+//console.log (retornaMaiorMenorNome (nomes));
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+//let numbers = [2, 3, 2, 5, 8, 2, 3];
+//let cont = 0;
+//function retornaMaisRepete (numbers){
+//    for (let index=0; index < numbers.length; index +=1) {
+//        for (let index2=0; index < numbers.length; index2 +=1)
+//           if (numbers[index] === numbers[index2]) {
+//                cont += 1; 
+//                }
+//            }
+//return cont;
+//}
+//console.log (retornaMaisRepete(numbers));
