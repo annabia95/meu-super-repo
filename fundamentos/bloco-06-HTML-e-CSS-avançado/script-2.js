f979fdf099f9c8a95af511007dbff5cd63dac64c
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-05 12:02:32 -0300

    Adicionando exercicio

---
+++ fundamentos/bloco-06-HTML-e-CSS-avançado/script-2.js
@@ -0,0 +1,43 @@
+let select = document.querySelector ('#select-estado');
+let estados = [
+    'AC - Acre',
+    'AL - Alagoas',
+    'AP - Amapá',
+    'AM - Amazonas',
+    'BA - Bahia',
+    'CE - Ceará',
+    'DF - Distrito Federal',
+    'ES - Espírito Santo',
+    'GO - Goías',
+    'MA - Maranhão',
+    'MT - Mato Grosso',
+    'MS - Mato Grosso do Sul',
+    'MG - Minas Gerais',
+    'PA - Pará',
+    'PB - Paraíba',
+    'PR - Paraná',
+    'PE - Pernambuco',
+    'PI - Piauí',
+    'RJ - Rio de Janeiro',
+    'RN - Rio Grande do Norte',
+    'RS - Rio Grande do Sul',
+    'RO - Rondônia',
+    'RR - Roraíma',
+    'SC - Santa Catarina',
+    'SP - São Paulo',
+    'SE - Sergipe',
+    'TO - Tocantins'
+    ];
+
+for (let i = 0; i < estados.length; i+= 1) {
+    let option = document.createElement ('option');
+    option.value = estados[i]
+    option.innerHTML = estados [i];
+    select.appendChild(option);
+}
+
+let bttnSubmit = document.getElementById ("button-1");
+bttnSubmit.addEventListener('click', (event) => {
+      event.preventDefault();
+}
+);
