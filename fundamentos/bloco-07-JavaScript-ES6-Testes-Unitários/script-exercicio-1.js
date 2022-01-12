bec7e1ac985b15c5d0c201f961660ed6a2983775
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-12 20:36:58 -0300

    Adicionando Exercicio 1

---
+++ fundamentos/bloco-07-JavaScript-ES6-Testes-Unitários/script-exercicio-1.js
@@ -0,0 +1,23 @@
+/* Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals .
+Copie o código abaixo. */
+/* function testingScope(escopo) {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true); */
+
+const testingScope = (escopo) => {
+  const testingMyEscopo =(escopo === true) ? `Não devo ser utilizada fora do meu escopo (if), ótimo, fui utilizada no escopo !` : `Não devo ser utilizada fora meu escopo (else)`;
+  console.log (testingMyEscopo);
+}
+testingScope (false);
