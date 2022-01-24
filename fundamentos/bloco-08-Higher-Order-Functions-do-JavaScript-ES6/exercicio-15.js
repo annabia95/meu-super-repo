21c7b8bddfcc7f70b1d5e85d87170281ad5bbff6
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-24 12:08:42 -0300

    Adicioanando exercicio

--- fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/exercicio-15.js
+++ fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/exercicio-15.js
@@ -30,3 +30,13 @@ const user = {
   const { name, age, nationality, profession, squad, squadInitials } = userInfos;
   
   console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[bebida, comida, animal] = ['água', 'arroz', 'gato']
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
