025f2c4458320994bfcc03bea973b4344fcaec97
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-01-24 14:19:46 -0300

    Adicioanando exercicio

--- fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/exercicio-15.js
+++ fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/exercicio-15.js
@@ -40,3 +40,40 @@ let bebida = 'arroz';
 console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+const getNationality = ({ firstName, nationality = "Brazilian" }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude, });
+
+console.log(getPosition(-19.8157, -43.9542));
+
+const multiply = (number, value = 2) => {
+  // Escreva aqui a sua função
+  return number * value
+};
+
+console.log(multiply(8));
